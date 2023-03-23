10cbdbd07d05a4e4d7f0ac7c03e6ad97b399d2c9
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-03-23 15:28:11 +0530

    changed test

--- tests/test_generate_view.py
+++ tests/test_generate_view.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 from automatic_view_updater.generate_view import generate_view
+import shutil
 
 def test_view():
     expected_view = Path("./tests/data/expected_result.txt").read_text(encoding="utf-8")
     result_views_path = generate_view("I3D4F1804","./data") 
     result_view = result_views_path[0].read_text(encoding="utf-8")
     assert expected_view == result_view
+    shutil.rmtree("./data")
