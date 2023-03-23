4f89f28ff7bb0266804b846a9e9d3b1c3adc01fb
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-03-23 16:31:44 +0530

    included dependencies

--- tests/test_generate_view.py
+++ tests/test_generate_view.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from automatic_view_updater.generate_view import generate_view
-import shutil
+import tempfile
 
 def test_view():
+    f = tempfile.TemporaryDirectory(dir = "./data")
     expected_view = Path("./tests/data/expected_result.txt").read_text(encoding="utf-8")
-    result_views_path = generate_view("I3D4F1804","./data") 
+    result_views_path = generate_view("I3D4F1804",f.name) 
     result_view = result_views_path[0].read_text(encoding="utf-8")
     assert expected_view == result_view
-    shutil.rmtree("./data")
+    f.cleanup()
