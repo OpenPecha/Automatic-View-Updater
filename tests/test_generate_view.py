18792b03caf09ff428133b8c32fd6bf66a6f3432
jungtop <jungtop@jungtops-MBP.bbrouter>
2023-03-27 19:16:17 +0530

    updated test

--- tests/test_generate_view.py
+++ tests/test_generate_view.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 from automatic_view_updater.generate_view import generate_view
+from automatic_view_updater.update_view import get_view_class
 import shutil
 
 def test_view():
 
     expected_view = Path("./tests/data/expected_result.txt").read_text(encoding="utf-8")
-    result_views_path = generate_view("I3D4F1804","./data") 
+    view = get_view_class("plaintext")
+    result_views_path = generate_view("I3D4F1804",view())
     result_view = result_views_path[0].read_text(encoding="utf-8")
     assert expected_view == result_view
-    shutil.rmtree("./data")
