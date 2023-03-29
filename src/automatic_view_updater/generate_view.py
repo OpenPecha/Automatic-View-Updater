5c51bf80ba955746f8c70b25ca78c95fa2e82f5b
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-03-29 10:23:04 +0530

    pre-commit successful

--- src/automatic_view_updater/generate_view.py
+++ src/automatic_view_updater/generate_view.py
@@ -1,35 +1,37 @@
+import re
+from pathlib import Path
 
 from collection.items.pecha import Pecha
 from collection.views.view import View
-from pathlib import Path
 from git import Repo
-from openpecha.utils import load_yaml
 from openpecha.config import BASE_PATH
-import re
+from openpecha.utils import load_yaml
 
 OPENPECHA_DATA_PREFIX_URL = "https://github.com/OpenPecha-Data"
 DEFAULT_OUTPUT_DIR = BASE_PATH
 
 
-
-def download_repo(item_id):
-    item_path = DEFAULT_OUTPUT_DIR.as_posix()+"/"+item_id
+def download_repo(item_id, output_dir):
+    item_path = output_dir.as_posix() + "/" + item_id
     item_github_url = f"{OPENPECHA_DATA_PREFIX_URL}/{item_id}"
-    Repo.clone_from(item_github_url,item_path)
+    Repo.clone_from(item_github_url, item_path)
     return item_path
 
-def get_item(item_id):
-    repo_path = download_repo(item_id)
+
+def get_item(item_id, output_dir):
+    repo_path = download_repo(item_id, output_dir)
     return repo_path
 
-def get_item_meta(item_id,item_path):
+
+def get_item_meta(item_id, item_path):
     meta = None
-    if re.match("^I",item_id):
+    if re.match("^I", item_id):
         meta_path = Path(f"{item_path}/{item_id}.opf/meta.yml")
         meta = load_yaml(meta_path)
     return meta
 
-def get_pecha_attr(dic,item_path)->None:
+
+def get_pecha_attr(dic, item_path):
     pecha = {}
     pecha_attrs = Pecha.__annotations__.keys()
     for pecha_attr in pecha_attrs:
@@ -41,18 +43,16 @@ def get_pecha_attr(dic,item_path)->None:
     return pecha
 
 
-def generate_view(item_id:str,view:View,output_dir:Path=None):
-    global DEFAULT_OUTPUT_DIR
+def generate_view(item_id: str, view: View, output_dir: Path = None):
     if output_dir:
-        DEFAULT_OUTPUT_DIR = output_dir
-    item_path = get_item(item_id)
-    meta = get_item_meta(item_id,item_path)
-    pecha_attr = get_pecha_attr(meta,item_path)
+        output_dir = BASE_PATH
+    item_path = get_item(item_id, output_dir)
+    meta = get_item_meta(item_id, item_path)
+    pecha_attr = get_pecha_attr(meta, item_path)
     pecha = Pecha(**pecha_attr)
-    views_path = view.serialize(pecha=pecha,output_dir=DEFAULT_OUTPUT_DIR)
+    views_path = view.serialize(pecha=pecha, output_dir=BASE_PATH)
     return views_path
 
 
 if __name__ == "__main__":
-    generate_view("I3D4F1804",Path("./data"))
-    
+    generate_view("I3D4F1804", Path("./data"))
