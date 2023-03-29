7e7deada2d7109356a6ffdddd9a72225c950a781
jungtop <jungtop@jungtops-MBP.bbrouter>
2023-03-29 14:02:33 +0530

    updated dir

--- src/automatic_view_updater/generate_view.py
+++ src/automatic_view_updater/generate_view.py
@@ -3,23 +3,24 @@ from pathlib import Path
 
 from collection.items.pecha import Pecha
 from collection.views.view import View
+from collection.views.plain_base import PlainBaseView
+from collection.collection.collection import Collection
 from git import Repo
 from openpecha.config import BASE_PATH
 from openpecha.utils import load_yaml
 
 OPENPECHA_DATA_PREFIX_URL = "https://github.com/OpenPecha-Data"
-DEFAULT_OUTPUT_DIR = BASE_PATH
 
 
-def download_repo(item_id, output_dir):
-    item_path = output_dir.as_posix() + "/" + item_id
+def download_repo(item_id):
+    item_path = BASE_PATH.as_posix() + "/" + item_id
     item_github_url = f"{OPENPECHA_DATA_PREFIX_URL}/{item_id}"
     Repo.clone_from(item_github_url, item_path)
     return item_path
 
 
-def get_item(item_id, output_dir):
-    repo_path = download_repo(item_id, output_dir)
+def get_item(item_id):
+    repo_path = download_repo(item_id)
     return repo_path
 
 
@@ -43,16 +44,17 @@ def get_pecha_attr(dic, item_path):
     return pecha
 
 
-def generate_view(item_id: str, view: View, output_dir: Path = None):
+def generate_view(item_id: str,output_dir: Path = None):
     if not output_dir:
         output_dir = BASE_PATH
-    item_path = get_item(item_id, output_dir)
+    item_path = get_item(item_id)
     meta = get_item_meta(item_id, item_path)
     pecha_attr = get_pecha_attr(meta, item_path)
     pecha = Pecha(**pecha_attr)
-    views_path = view.serialize(pecha=pecha, output_dir=BASE_PATH)
-    return views_path
+    col = Collection(title="213",items=[pecha],parent_dir=output_dir,views=[PlainBaseView()])
+    col.save_collection()
+    #views_path = view.serialize(pecha=pecha, output_dir=BASE_PATH)
 
 
 if __name__ == "__main__":
-    generate_view("I3D4F1804", Path("./data"))
+    generate_view("I3D4F1804",Path("./data"))
