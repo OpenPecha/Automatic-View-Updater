5c8de391f2547b0795dee48ccfa0a28799f9d14c
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-04-06 12:22:45 +0530

    fixed minor issue

--- src/automatic_view_updater/generate_view.py
+++ src/automatic_view_updater/generate_view.py
@@ -1,27 +1,26 @@
 import re
 from pathlib import Path
-from collection.items.pecha import Pecha
+
+from collection.items.alignment import Alignment
 from collection.items.collection_meta import CollectionMeta
+from collection.items.pecha import Pecha, PechaFragment
+from collection.items.work import Work
 from collection.utils import get_item
+from collection.views.plain_text import PlainTextView
+from collection.views.view import View
 from openpecha.config import BASE_PATH
 from openpecha.utils import load_yaml
-from collection.items.pecha import Pecha,PechaFragment
-from collection.items.alignment import Alignment
-from collection.items.work import Work
-from collection.views.plain_text import PlainTextView
-from enum import Enum
-from collection.views.view import View
-import os
 
 
-class ItemEnum():
-    pecha =  Pecha
+class ItemEnum:
+    pecha = Pecha
     alignment = Alignment
     work = Work
     pecha_fragment = PechaFragment
 
 
 OPENPECHA_DATA_PREFIX_URL = "https://github.com/OpenPecha-Data"
+
 
 def get_op_item_meta(item_id, item_path):
     meta = None
@@ -42,12 +41,14 @@ def get_item_attr(dic, item_path):
     pecha["path"] = item_path
     return pecha
 
+
 def get_meta(col):
-    meta = CollectionMeta(collection_id=col.id,item_views_map=col.item_views_map)
+    meta = CollectionMeta(collection_id=col.id, item_views_map=col.item_views_map)
     return meta
 
+
 def get_item_cls(item_id):
-    
+
     if item_id.startswith("A"):
         item_class = ItemEnum.alignment
     elif item_id.startswith("W"):
@@ -61,10 +62,10 @@ def get_collection_meta(collection_id):
     meta_path = Path(f"{collection_id}.opc/meta.yml")
     meta = load_yaml(meta_path)
     return meta
-        
 
-def generate_view(op_item_id: str,view:View,output_dir: Path = None):
-    Path("./data").mkdir(parents=True,exist_ok=True)
+
+def generate_view(op_item_id: str, view: View, output_dir: Path = None):
+    Path("./data").mkdir(parents=True, exist_ok=True)
     if output_dir is None:
         output_dir = BASE_PATH
     op_item_path = get_item(op_item_id)
@@ -73,9 +74,10 @@ def generate_view(op_item_id: str,view:View,output_dir: Path = None):
     item = get_item_cls(op_item_id)
     item_obj = item(**item_attr)
     serializer = view.serializer
-    views_path = serializer().serialize(item=item_obj,output_dir=Path("./data"))
+    views_path = serializer().serialize(item=item_obj, output_dir=Path("./data"))
 
     return views_path
 
+
 if __name__ == "__main__":
-    generate_view("I3D4F1804",PlainTextView(),Path("./data"))
+    generate_view("I3D4F1804", PlainTextView(), Path("./data"))
--- tests/test_generate_view.py
+++ tests/test_generate_view.py
@@ -7,7 +7,7 @@ from automatic_view_updater.generate_view import BASE_PATH, generate_view
 def test_view():
 
     expected_view = Path("./tests/data/expected_result.txt").read_text(encoding="utf-8")
-    result_views_path = generate_view("I3D4F1804",Path("./data"))
+    result_views_path = generate_view("I3D4F1804", Path("./data"))
     first_view = result_views_path[0]
     result_view = first_view.read_text(encoding="utf-8")
     assert expected_view == result_view
