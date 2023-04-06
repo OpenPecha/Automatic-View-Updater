16f0eef861920f894d594e1cabc3aeee6b258f81
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-04-06 12:24:27 +0530

    updated test

--- tests/test_generate_view.py
+++ tests/test_generate_view.py
@@ -1,13 +1,13 @@
 import shutil
 from pathlib import Path
-
+from collection.views.plain_text import PlainTextView
 from automatic_view_updater.generate_view import BASE_PATH, generate_view
 
 
 def test_view():
 
     expected_view = Path("./tests/data/expected_result.txt").read_text(encoding="utf-8")
-    result_views_path = generate_view("I3D4F1804", Path("./data"))
+    result_views_path = generate_view("I3D4F1804",PlainTextView(),Path("./data"))
     first_view = result_views_path[0]
     result_view = first_view.read_text(encoding="utf-8")
     assert expected_view == result_view
